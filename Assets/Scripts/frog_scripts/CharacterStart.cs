ed415309187285ca41b77ca8959fa8ba1862cd79
mindlessmedic <mindlessmendicant@gmail.com>
2020-03-06 13:17:16 -0500

    fixing the fuckup2 +turns :D

---
+++ Assets/Scripts/frog_scripts/CharacterStart.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CharacterStart : MonoBehaviour
+{
+    public Animator anim;
+    public bool HasTurn;
+    public SimpleController SC;
+    public GameObject CVC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            CVC.SetActive(true);
+        }
+    }
+
+    public void CharacterBegin()
+    {
+        SC.enabled = true;
+    }
+
+    public void CharacterTurn()
+    {
+        if (HasTurn == false)
+        {
+            anim.SetBool("turn", true);
+            HasTurn = true;
+        }
+
+        else if (HasTurn == true)
+        {
+            anim.SetBool("turn", false);
+            HasTurn = false;
+        }
+    }
+}

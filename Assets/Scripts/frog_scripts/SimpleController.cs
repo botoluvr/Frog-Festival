004d79b4822c10f23fb50cd1f533f06bd2cc7d21
botoluvr <luvaboto@gmail.com>
2020-04-10 00:38:31 -0400

    oops i fixed it
    moved all the scripts to the froggy folder..........

--- Assets/Scripts/frog_scripts/SimpleController.cs
+++ Assets/Scripts/frog_scripts/SimpleController.cs
@@ -8,11 +8,15 @@ public class SimpleController : MonoBehaviour
 {
     public bool turn;
     public Rigidbody2D myRigidBody;
-    public float movespeed;
+    public float moveSpeed;
+
+    public LevelController LC;
+    public Vector3 respawnPosition;
+    public CharacterStart cs;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,24 +24,70 @@ public class SimpleController : MonoBehaviour
     {
         if (turn == true)
         {
-            myRigidBody.velocity = new Vector2(-movespeed, 0);
+            myRigidBody.velocity = new Vector2(0, moveSpeed);
         }
 
         if (turn == false)
         {
-            myRigidBody.velocity = new Vector2(0, movespeed);
+            myRigidBody.velocity = new Vector2(-moveSpeed, 0);
         }
     }
 
-    public void turnfrog()
+    public void turnbird()
     {
-        if (turn == true)
+        if (cs.alive == true)
         {
-            turn = false;
+            if (turn == true)
+            {
+                turn = false;
+            }
+            else if (turn == false)
+            {
+                turn = true;
+            }
         }
-        else if (turn == false)
-        {
-            turn = true;
-        }
+
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // tells the game to play the section of the song associated with each checkpoint
+        if (collision.tag == "Checkpoint1")
+        {
+            respawnPosition = collision.transform.position;
+            LC.currentsong.Stop();
+            LC.currentsong = LC.song2;
+            LC.currentsong.Play();
+        }
+
+        if (collision.tag == "Checkpoint2")
+        {
+            respawnPosition = collision.transform.position;
+            LC.currentsong.Stop();
+            LC.currentsong = LC.song3;
+            LC.currentsong.Play();
+        }
+
+        if (collision.tag == "Checkpoint3")
+        {
+            respawnPosition = collision.transform.position;
+            LC.currentsong.Stop();
+            LC.currentsong = LC.song4;
+            LC.currentsong.Play();
+        }
+
+        // sets the respawn reaction for hitting a wall
+        if (collision.tag == "KillPlane")
+        {
+            //gameObject.SetActive (false);
+            //transform.position = respawnPosition;
+            if (LC.respawning == false)
+            {
+                LC.Respawn();
+            }
+
+        }
+
+    }
+
 }

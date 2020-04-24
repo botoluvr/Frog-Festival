cc568caa4413f464f4976abe8231027112659c27
mindlessmedic <mindlessmendicant@gmail.com>
2020-04-24 07:23:30 -0400

    level select animation

---
+++ Assets/Scripts/frog_scripts/songselect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class songselect : MonoBehaviour
+{
+    public Animator Levels;
+    public bool leftRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LevelTwo()
+    {
+        if (leftRight == false)
+        {
+            Levels.SetBool("LeftRight", true);
+            leftRight = true;
+        }
+
+       else if (leftRight == true)
+        {
+            Levels.SetBool("LeftRight", false);
+            leftRight = false;
+        }
+    }
+}
